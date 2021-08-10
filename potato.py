a77d6ea4fd7c5143967ace26f1e752d3ba526feb
Andres P <apating733@student.fuhsd.org>
2021-08-10 15:16:14 -0700

    feat: added NotPotato function

--- potato.py
+++ potato.py
@@ -6,4 +6,13 @@ def potato():
             n += 1
         else:
             break
+def NotPotato():
+    while True:
+        for num in range(100):
+            if num%5 ==0:
+                print(num)
+        break
 potato()
+print("\n\n\n")
+NotPotato()
+
